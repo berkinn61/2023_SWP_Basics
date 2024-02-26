a3bb94198b9a1be824f1009c1486134444848a0d
berkinn61 <119922590+berkinn61@users.noreply.github.com>
2024-02-26 12:20:46 +0100

    ran commit Aufgabe 11

---
+++ 11_Arays/demo.js
@@ -0,0 +1,25 @@
+let a = "susi";
+let b = "paula";
+let c = "hans";
+
+let arr = ["Susi", "paul", "hans"];
+console.log(arr[0]);
+
+arr.push(10);
+arr.push(3);
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+    
+// }
+
+
+
+// Erstelle ein Array mit folgenden Strings:
+// "Susi", "Paula", "Hans"
+
+// Gib folgenden Satz aus:
+// "Meine Freunde sind Susi, Paula und Hans
+
+// Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten:
+// "Meine Freunde sind Susi, Paula, Hans und Sepp
